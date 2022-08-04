785e5318663a6ef815c9cd0042c4c9a62a98df35
Anas <anaskhafegui@Anas.localdomain>
2022-08-04 13:39:37 +0200

    install CRUD opreations for prodcut by trestle package by command rails generate trestle:resource Product

--- db/migrate/20220804111641_create_products.rb
+++ db/migrate/20220804111641_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name
-      t.decimal :price
-      t.string :image
-      t.string :description
+      t.string :name, :string
+      t.decimal :price, :decimal, :precision => 8, :scale => 2
+      t.string :image, :string
+      t.string :description, :string
       t.string :created_at
       t.string :updated_at
       t.timestamps
